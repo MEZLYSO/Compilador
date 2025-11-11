96baf9f15c0734cfeeb536a23cd47168ccff3370
Maury SO <lynuxazura@gmail.com>
2025-11-11 15:46:15 -0600

    Finalizando API y Grammar

---
+++ proyect/api/utils.py
@@ -0,0 +1,28 @@
+from antlr4 import *
+import lenguage.GrammarLexer as GrammarLexer
+import lenguage.GrammarParser as GrammarParser
+import  io
+import sys
+import lenguage.MyVisitor as MyVisitor
+
+def run_code(code:str):
+    input_stream=InputStream(code)
+    lexer=GrammarLexer(input_stream)
+    stream=CommonTokenStream(lexer)
+    parser=GrammarParser(stream)
+    tree=parser.program()
+
+    # Capturan la salida 
+    old_stdout=sys.stdout()
+    buf = io.StringIO()
+    sys.stdout = buf
+
+    #Creamo un objeto de nuestro visitor
+    visitor = MyVisitor()
+    #Visitamos el arbol con nuestro visitor
+    visitor.visit(tree)
+    # Capturamos la salida
+    output=buf.getvalue()
+
+    return output
+
